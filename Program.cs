672cfccf78edd3f6ce2cb57f5b49b3340e316294
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-08 20:33:34 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("Ousadia e alegria - Neymar e Thiaguinho");
+Console.ResetColor();
+Console.WriteLine("");
+
+Console.WriteLine("Chego chegando, beijo no canto da boca");
+Console.WriteLine("Tumultuando, ai meu Deus que coisa louca");
+Console.WriteLine("Se der espaço, eu pedalo e vou pra cima");
+Console.WriteLine("Corro pro abraço, vacilo entro no clima.");
+Console.WriteLine("O nosso lema é ousadia e alegria");
+Console.WriteLine("A nossa cara é pagode todo dia");
+Console.WriteLine("Cheio de estilo na pressão eu vou com tudo");
+Console.WriteLine("Só espalhando ousadia pelo o mundo.");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Lê lêlê, lêlê, lêlê");
+Console.WriteLine("Ousadia pra vencer");
+Console.WriteLine("Lê lêlê, lêlê, lêlê");
+Console.WriteLine("Alegria pra viver.");
+Console.WriteLine("Daquele jeito");
+Console.ResetColor();
+Console.WriteLine("");
